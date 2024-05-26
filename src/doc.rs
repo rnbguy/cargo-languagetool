40e135d9f5b0c2e5dd6aea86fb16751de4cd3942
Ranadeep Biswas <mail@rnbguy.at>
2024-05-26 21:44:23 +0200

    refactor: rm redundant HashMap

--- src/doc.rs
+++ src/doc.rs
@@ -1,7 +1,5 @@
 //! The `docs` module contains all the necessary stuff to work with doc comments.
 
-use std::collections::HashMap;
-
 use annotate_snippets::{Level, Renderer, Snippet};
 use color_eyre::eyre::ContextCompat;
 use color_eyre::{Report, Result};
@@ -9,48 +7,29 @@ use languagetool_rust::CheckResponse;
 use log::debug;
 
 #[derive(Debug, Clone)]
-pub struct Docs(HashMap<String, Vec<proc_macro2::Literal>>);
+pub struct Docs(pub Vec<proc_macro2::Literal>);
 
-impl Docs {
-    fn append(&mut self, docs: Self) {
-        docs.0.into_iter().for_each(|(key, mut value)| {
-            self.0.entry(key).or_default().append(&mut value);
-        });
-    }
-
-    #[must_use]
-    pub fn is_empty(&self) -> bool {
-        self.0.is_empty()
-    }
-}
-
-impl<T> From<(T, proc_macro2::TokenStream)> for Docs
-where
-    T: AsRef<str>,
-{
-    fn from(stream: (T, proc_macro2::TokenStream)) -> Self {
+impl From<proc_macro2::TokenStream> for Docs {
+    fn from(stream: proc_macro2::TokenStream) -> Self {
         use proc_macro2::TokenTree;
 
-        let mut docs = Self(HashMap::new());
+        let mut docs = vec![];
         let mut is_doc = false;
-        for tree in stream.1 {
+        for tree in stream {
             match tree {
                 TokenTree::Ident(ident) => is_doc = ident == "doc",
                 TokenTree::Group(group) => {
-                    docs.append(Self::from((stream.0.as_ref().to_owned(), group.stream())));
+                    docs.append(&mut Self::from(group.stream()).0);
                 }
                 TokenTree::Literal(literal) => {
                     if is_doc {
-                        docs.0
-                            .entry(stream.0.as_ref().to_owned())
-                            .or_default()
-                            .push(literal);
+                        docs.push(literal);
                     }
                 }
                 TokenTree::Punct(_) => {}
             };
         }
-        docs
+        Self(docs)
     }
 }
 
@@ -92,12 +71,12 @@ pub struct FixedDoc {
 }
 
 impl core::fmt::Display for FixedDoc {
-    fn fmt(&self, formatter: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         let mut iter = self.text.iter();
         if let Some((first_string, _)) = iter.next() {
-            write!(formatter, "{first_string}")?;
+            write!(f, "{first_string}")?;
             for (fixed_string, _) in iter {
-                write!(formatter, "\n{fixed_string}")?;
+                write!(f, "\n{fixed_string}")?;
             }
         }
         Ok(())
@@ -172,7 +151,7 @@ impl FixedDoc {
 #[derive(Debug, Clone)]
 pub struct FixedDocs {
     pub original: Docs,
-    pub fixed: HashMap<String, Vec<FixedDoc>>,
+    pub fixed: Vec<FixedDoc>,
 }
 
 // fn fix_string(s: &str) -> String {
@@ -188,47 +167,46 @@ impl TryFrom<Docs> for FixedDocs {
     type Error = Report;
 
     fn try_from(original: Docs) -> Result<Self> {
-        let mut fixed: HashMap<String, Vec<FixedDoc>> = HashMap::new();
-
-        for (file, docs) in &original.0 {
-            for doc in docs {
-                let (original_string, span) = {
-                    let original_string: String = serde_json::from_str(&doc.to_string())?;
-                    let mut span: FixedDocSpan = doc.span().into();
-                    match original_string.strip_prefix(' ') {
-                        Some(fixed_string) => {
-                            span.start.column += 1;
-                            (fixed_string.to_owned(), span)
+        let fixed =
+            original
+                .0
+                .iter()
+                .try_fold::<_, _, Result<_>>(Vec::new(), |mut fixed_docs, doc| {
+                    let (original_string, span) = {
+                        let original_string: String = serde_json::from_str(&doc.to_string())?;
+                        let mut span: FixedDocSpan = doc.span().into();
+                        match original_string.strip_prefix(' ') {
+                            Some(fixed_string) => {
+                                span.start.column += 1;
+                                (fixed_string.to_owned(), span)
+                            }
+                            None => (original_string, span),
                         }
-                        None => (original_string, span),
-                    }
-                };
+                    };
 
-                if fixed.contains_key(file) {
-                    let fixed_docs = fixed.get_mut(file).context("No doc")?;
-                    let last = fixed_docs.last_mut().context("No last doc")?;
-
-                    // If the lines are consecutive, then these two doc comments belong to a single block.
-                    if span.start.line - last.text.last().context("must have one")?.1.end.line == 1
-                    {
-                        last.text.push((original_string.clone(), span));
-                    } else {
+                    if fixed_docs.is_empty() {
                         fixed_docs.push(FixedDoc {
-                            text: vec![(original_string.clone(), span)],
+                            text: vec![(original_string, span)],
                             check_response: None,
                         });
+                    } else {
+                        let last = fixed_docs.last_mut().context("No last doc")?;
+
+                        // If the lines are consecutive, then these two doc comments belong to a single block.
+                        if span.start.line - last.text.last().context("must have one")?.1.end.line
+                            == 1
+                        {
+                            last.text.push((original_string, span));
+                        } else {
+                            fixed_docs.push(FixedDoc {
+                                text: vec![(original_string, span)],
+                                check_response: None,
+                            });
+                        }
                     }
-                } else {
-                    fixed.insert(
-                        file.clone(),
-                        vec![FixedDoc {
-                            text: vec![(original_string.clone(), span)],
-                            check_response: None,
-                        }],
-                    );
-                }
-            }
-        }
+
+                    Ok(fixed_docs)
+                })?;
 
         Ok(Self { original, fixed })
     }
--- src/utils.rs
+++ src/utils.rs
@@ -13,7 +13,7 @@ use crate::doc::{Docs, FixedDoc, FixedDocs};
 ///
 /// # Errors
 /// If an error occurs.
-pub fn fetch_docs(dir: &PathBuf) -> Result<Vec<Docs>> {
+pub fn fetch_docs(dir: &PathBuf) -> Result<Vec<(String, Docs)>> {
     use proc_macro2::TokenStream;
 
     walkdir::WalkDir::new(dir)
@@ -31,9 +31,9 @@ pub fn fetch_docs(dir: &PathBuf) -> Result<Vec<Docs>> {
         .map(|path| {
             let content = std::fs::read_to_string(&path)?;
             let stream: TokenStream = syn::parse_str(&content)?;
-            Ok(Docs::from((&path, stream)))
+            Ok((path, Docs::from(stream)))
         })
-        .filter(|docs| docs.as_ref().map(Docs::is_empty).ok() != Some(true))
+        .filter(|result| result.as_ref().map(|(_, docs)| !docs.0.is_empty()).ok() != Some(true))
         .collect::<Result<_>>()
 }
 
@@ -126,101 +126,95 @@ fn docs_checked(
     config: &Config,
     docs: &mut FixedDocs,
 ) -> Result<()> {
-    for docs in docs.fixed.values_mut() {
-        for doc in docs {
-            doc_checked(server, config, doc)?;
-        }
+    for doc in &mut docs.fixed {
+        doc_checked(server, config, doc)?;
     }
 
     Ok(())
 }
 
 /// Pretty-printer.
-fn print_docs(file_docs: &FixedDocs) -> Result<()> {
-    for (file, docs) in &file_docs.fixed {
-        let source = std::fs::read_to_string(file)?;
-        for doc in docs {
-            if let Some(check_response) = &doc.check_response {
-                FixedDoc::annotate(file, &source, check_response);
-            }
+fn print_docs(file: &str, docs: &FixedDocs) -> Result<()> {
+    let source = std::fs::read_to_string(file)?;
+    for doc in &docs.fixed {
+        if let Some(check_response) = &doc.check_response {
+            FixedDoc::annotate(file, &source, check_response);
         }
     }
 
     Ok(())
 }
 
-fn transform_matches(docs: &mut FixedDocs) -> Result<()> {
-    for (file, docs) in &mut docs.fixed {
-        for doc in docs {
-            let doc_str = doc.to_string();
-            if let Some(check_response) = doc.check_response.as_mut() {
-                for each_match in &mut check_response.matches {
-                    let file_str = std::fs::read_to_string(file)?;
+fn transform_matches(file: &str, docs: &mut FixedDocs) -> Result<()> {
+    for doc in &mut docs.fixed {
+        let doc_str = doc.to_string();
+        if let Some(check_response) = doc.check_response.as_mut() {
+            for each_match in &mut check_response.matches {
+                let file_str = std::fs::read_to_string(file)?;
 
-                    // ensured by LT API.
-                    // assert_eq!(each_match.length, each_match.context.length);
+                // ensured by LT API.
+                // assert_eq!(each_match.length, each_match.context.length);
 
-                    let row = doc_str
-                        .chars()
-                        .take(each_match.offset)
-                        .filter(|chr| chr == &'\n')
-                        .count();
+                let row = doc_str
+                    .chars()
+                    .take(each_match.offset)
+                    .filter(|chr| chr == &'\n')
+                    .count();
 
-                    let (_line, span) = &doc.text[row];
+                let (_line, span) = &doc.text[row];
 
-                    let doc_prev_line_end_offset = doc_str
-                        .lines()
-                        .take(row)
-                        .map(|st| st.len() + 1) // because of extra space
-                        .sum::<usize>();
+                let doc_prev_line_end_offset = doc_str
+                    .lines()
+                    .take(row)
+                    .map(|st| st.len() + 1) // because of extra space
+                    .sum::<usize>();
 
-                    // offset of the match in the line in doc comments
-                    let doc_line_offset = each_match.offset - doc_prev_line_end_offset;
+                // offset of the match in the line in doc comments
+                let doc_line_offset = each_match.offset - doc_prev_line_end_offset;
 
-                    let line_row = span.start.line;
-                    let line_offset = span.start.column + 3 + doc_line_offset; // because of rust comment tags
+                let line_row = span.start.line;
+                let line_offset = span.start.column + 3 + doc_line_offset; // because of rust comment tags
 
-                    // line beginning in the file
-                    let line_begin_offset = file_str
-                        .lines()
-                        .take(line_row - 1)
-                        .map(|st| st.len() + 1)
-                        .sum::<usize>();
+                // line beginning in the file
+                let line_begin_offset = file_str
+                    .lines()
+                    .take(line_row - 1)
+                    .map(|st| st.len() + 1)
+                    .sum::<usize>();
 
-                    let doc_match_offset = each_match.offset;
+                let doc_match_offset = each_match.offset;
 
-                    // updating value
-                    each_match.offset = line_begin_offset + line_offset;
+                // updating value
+                each_match.offset = line_begin_offset + line_offset;
 
-                    // LT context starts at: each_match.offset - each_match.context.offset
-                    // start the context from the same line as the beginning of the match.
-                    each_match.context.offset = line_offset; // this gets changed too
+                // LT context starts at: each_match.offset - each_match.context.offset
+                // start the context from the same line as the beginning of the match.
+                each_match.context.offset = line_offset; // this gets changed too
 
-                    // end the context at the end of the line of the end of the match.
+                // end the context at the end of the line of the end of the match.
 
-                    let mut new_context_length = 0;
-                    let mut length_delta = 0;
-                    let mut match_count = doc_prev_line_end_offset;
+                let mut new_context_length = 0;
+                let mut length_delta = 0;
+                let mut match_count = doc_prev_line_end_offset;
 
-                    for (doc_line, file_line) in doc_str
-                        .lines()
-                        .skip(row)
-                        .zip(file_str.lines().skip(line_row - 1))
-                    {
-                        new_context_length += file_line.len() + 1; // because of newline
-                        match_count += doc_line.len() + 1; // because of newline
-                        if doc_match_offset + each_match.length < match_count {
-                            break;
-                        }
-                        length_delta += span.start.column + 3; // because of rust comment tags
+                for (doc_line, file_line) in doc_str
+                    .lines()
+                    .skip(row)
+                    .zip(file_str.lines().skip(line_row - 1))
+                {
+                    new_context_length += file_line.len() + 1; // because of newline
+                    match_count += doc_line.len() + 1; // because of newline
+                    if doc_match_offset + each_match.length < match_count {
+                        break;
                     }
-
-                    each_match.length += length_delta;
-                    each_match.context.length = each_match.length;
-
-                    file_str[line_begin_offset..][..new_context_length]
-                        .clone_into(&mut each_match.context.text);
+                    length_delta += span.start.column + 3; // because of rust comment tags
                 }
+
+                each_match.length += length_delta;
+                each_match.context.length = each_match.length;
+
+                file_str[line_begin_offset..][..new_context_length]
+                    .clone_into(&mut each_match.context.text);
             }
         }
     }
@@ -232,16 +226,16 @@ fn transform_matches(docs: &mut FixedDocs) -> Result<()> {
 ///
 /// # Errors
 /// If an error occurs.
-pub fn check_grammar<I: IntoIterator<Item = Docs>>(
+pub fn check_grammar<I: IntoIterator<Item = (String, Docs)>>(
     server: &languagetool_rust::ServerClient,
     config: &Config,
-    docs: I,
+    files: I,
 ) -> Result<()> {
-    for doc in docs {
+    for (file, doc) in files {
         let mut fixed_doc = FixedDocs::try_from(doc)?;
         docs_checked(server, config, &mut fixed_doc)?;
-        transform_matches(&mut fixed_doc)?;
-        print_docs(&fixed_doc)?;
+        transform_matches(&file, &mut fixed_doc)?;
+        print_docs(&file, &fixed_doc)?;
     }
 
     Ok(())
